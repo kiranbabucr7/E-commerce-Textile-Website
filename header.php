d9da80029a29f9f74d7cffa73bb7637bacadced1
kiranbabucr7 <kiranbabu716@gmail.com>
2020-07-29 17:57:10 +0530

    started doing header.php

---
+++ header.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!-- font awsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Square&display=swap" rel="stylesheet">
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <header>
+      <div class="header_scetion_1">
+        <div class="container text-center">
+          <p class="header_scetion_1_text">FREE DELIVERY ANYWHERE INDIA</p>
+        </div>
+      </div>
+      <div class="header_scetion_2">
+        <div>
+          <div class="container">
+            <div>
+              <div class="row">
+                <div class="col-md-4">
+                  <div class="header-search-form ml-3 pt-md-4">
+                    <form>
+                      <div class="form-group">
+                        <div class="d-flex justify-content-center justify-content-md-start">
+                          <div>
+                            <input type="text" class="form-control" id="header_search_text" aria-describedby="emailHelp" placeholder="Search your keywords here">
+                          </div>
+                          <div>
+                            <button class="btn header_search_btn" type="submit"><i class="fa fa-search"></i></button>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="d-flex justify-content-center">
+                    <img src="assets/logo.png" alt="aakrithi" style="width: 70px;">
+                  </div>
+                  <div class="d-flex justify-content-center">
+                    <img src="assets/0668939e85df230834c67f739ed3e9ad.png" alt="aakrithi" style="width: 70px;">
+                  </div>
+                  <div class="d-flex justify-content-center">
+                    <p class="logo_text">KURTHIS</p>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="pt-md-4">
+                    <div class="d-flex justify-content-center justify-content-md-end">  
+                      <a href="#">login</a>
+                      <p class="m-0">/</p>
+                      <a href="#">Register</a>
+                    </div>
+                    <div class="d-flex justify-content-center justify-content-md-end">  
+                      <p>Shopping Bag</p>
+                      <p class="header_shoppingbag_price pl-2">Rs. 0.00</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
