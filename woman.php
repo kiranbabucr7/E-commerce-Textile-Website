6d71e044cbf88935170f3159620ee5af79251dda
kiranbabucr7 <kiranbabu716@gmail.com>
2020-08-03 18:24:51 +0530

    updated woman.php

--- woman.php
+++ woman.php
@@ -19,14 +19,98 @@
     <?php include 'header.php' ?>
     <main>
       <div class="container">
-        hai
+        <hr>
+        <div class="d-flex">
+          <a href="index.php">HOME</a>
+          <p class="pl-2 pr-2">/</p>
+          <a href="#">FESTIVE</a>
+        </div>
+        <div class="woman-main-body mt-3">
+          <div class="row">
+            <div class="col-md-3">
+              <div>
+                <div class="catogory">
+                  <div class="catogory-header" data-toggle="collapse" href="#catogory-body" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    <div class="d-flex">
+                      <div class="pt-2">
+                        <div class="burger mb-1"></div>
+                        <div class="burger mb-1"></div>
+                        <div class="burger mb-1"></div>
+                      </div>
+                      <div>
+                        <p class="catogory-p pl-2">Category</p>
+                      </div>
+                    </div>
+                    <hr class="m-0 mb-3">
+                  </div>
+                  <div class="category-accordion">
+                    <div id="accordion">
+                      <div class="catogory-one mb-2">
+                        <a href="#" data-toggle="collapse" data-target="#categoryOne" aria-expanded="true" aria-controls="categoryOne">
+                          Woman
+                        </a>
+                        <div id="categoryOne" class="collapse show" data-parent="#accordion">
+                          <div>
+                            <ul class="list-unstyled pl-4">
+                              <li><a href="#">Sarees</a></li>
+                              <li><a href="#">Dresses & Gowns</a></li>
+                              <li><a href="#">Anarkali & Suits</a></li>
+                              <li><a href="#">Kurtas & Suit sets</a></li>
+                              <li><a href="#">Lehengas</a></li>
+                              <li><a href="#">Tops</a></li>
+                              <li><a href="#">Bottom</a></li>
+                            </ul>
+                          </div>
+                        </div> 
+                      </div>                    
+                      <div class="catogory-two mb-2">
+                        <a href="#" data-toggle="collapse" data-target="#categoryTwo" aria-expanded="false" aria-controls="categoryTwo">
+                          New Look
+                        </a>
+                        <div id="categoryTwo" class="collapse" data-parent="#accordion">
+                          <div>
+                            <ul class="list-unstyled pl-4">
+                              <li><a href="#">Anarkali & Suits</a></li>
+                              <li><a href="#">Kurtas & Suit sets</a></li>
+                              <li><a href="#">Lehengas</a></li>
+                              <li><a href="#">Tops</a></li>
+                              <li><a href="#">Sarees</a></li>
+                              <li><a href="#">Dresses & Gown</a></li>
+                            </ul>
+                          </div>
+                        </div> 
+                      </div>  
+                      <div class="catogory-three mb-2">
+                        <a href="#" data-toggle="collapse" data-target="#categoryThree" aria-expanded="false" aria-controls="categoryThree">
+                          Festive
+                        </a>
+                        <div id="categoryThree" class="collapse" data-parent="#accordion">
+                          <div>
+                            <ul class="list-unstyled pl-4">
+                              <li><a href="#">Anarkali & Suits</a></li>
+                              <li><a href="#">Kurtas & Suit sets</a></li>
+                              <li><a href="#">Lehengas</a></li>
+                              <li><a href="#">Tops</a></li>
+                              <li><a href="#">Sarees</a></li>
+                              <li><a href="#">Dresses & Gown</a></li>
+                            </ul>
+                          </div>
+                        </div> 
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
     <?php include 'footer.php' ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
